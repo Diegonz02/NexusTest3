19d27d9c1f833c5c3f8c15e87c5083d4e00f5a6c
JuanZarate-rgb <Zaratejuan805@gmail.com>
2022-03-17 19:23:55 -0700

    Search bar

--- app/src/main/java/com/example/nexustest3/HistoricalData.java
+++
@@ -1,9 +0,0 @@
-package com.example.nexustest3;
-
-public class HistoricalData  extends Thread{
-    private static final String BASE_URL = "https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=AMD%2CIBM%2CAAPL";
-    private static final String QUERY = "q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22";
-    private static final String FORMAT = "&format=json";
-    private static final String DIAGNOSTICS = "&diagnostics=true";
-    private static final String ENV = "&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-}
