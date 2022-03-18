f19c8cb69403804cdb7b7587da8f648262aef91a
Diego Gonzales <diego.gonzales963@myci.csuci.edu>
2022-03-17 19:45:00 -0700

    Data for the learn fragment done.

--- app/src/main/java/com/example/nexustest3/Data.java
+++ app/src/main/java/com/example/nexustest3/Data.java
@@ -2,7 +2,63 @@ package com.example.nexustest3;
 
 public class Data {
     public static String[] names= new String[]{
-            "Android", "Java" ,"C","C++",
-            "Php"
+            "Intro: What is a stock \n\n" +
+                    "There are two different types of stocks: common and preferred.\n" +
+                    "Common stock represents ownership, so if a friend tells you they own stock, this " +
+                    "is common stock.\n" +
+                    "Preferred Stock is the debt instrument of a company. This is like loaning money " +
+                    "to the company.\n" +
+                    "A stock = a share \n" +
+                    "You have a claim on a company's assets and earnings.\n" +
+                    "A stock = Equity \n" +
+                    "You raise money for business to grow. \n" +
+                    "Choosing Brokerage \n" +
+                    "A brokerage is a firm that acts as a broker. Here we can buy and sell stocks of " +
+                    "companies, or other securities. \n" +
+                    "Brokerage examples: TD Ameritrade, Robinhood, E*Trade \n" +
+                    "TD Ameritrade has the following: Great customer service, No minimum money needed, " +
+                    "for traders and investors, etc. \n" +
+                    "Robinhood has the following: User friendly, no minimum money needed, crypto, etc. \n" +
+                    "E*Trade has the following: Customer Service, easy to use, etc. \n",
+            "What to look for in a company \n\n" +
+                    "Here are some questions you should ask yourself before buying shares of a company: \n" +
+                    "What do they do? \n" +
+                    "What do they make? \n" +
+                    "Who are the competitors? \n" +
+                    "Who are the clients? \n" +
+                    "How's production? \n" +
+                    "Do they need raw materials?, Where and how much?, Supply and demand?" ,
+            "Sectors \n\n" +
+                    "Sectors are a broad grouping of companies that have similar economic characteristics \n" +
+                    "There are 11 sectors \n" +
+                    "Financials - Banks, insurance companies, investment funds. EX. $GS $JPM \n" +
+                    "Utilities - Electric, Gas, water companies. EX. $NEE, $ENB, $DUK \n" +
+                    "Consumer Cyclical - Retailers, media companies, apparel companies. EX. $AMZN $NKE \n" +
+                    "Consumer Staples - Food and beverage companies. EX. $COST $MKC \n" +
+                    "Technology - From software to consumer electronics to social media. EX. $AMD $AAPL \n " +
+                    "Energy - Oil and gas exploration. $WMB $DVN \n" +
+                    "Healthcare - biotechnology companies, hospital management firms. EX. $CVS $BMY \n",
+            "Sectors Continued\n\n" +
+                    "Industrial - aerospace, defense, machinery, construction. EX. $CAT $LMT\n" +
+                    "Communication Services - wireless providers, cable companies. EX. $VZ $T\n" +
+                    "Materials - mining, refining, chemical. EX. $VALE $BHP" +
+                    "Real Estate - Companies invested in residential, industrial, and retail. $OLD, $MFA",
+            "Quarterly Earnings Report \n\n" +
+                    "A Quarterly Earnings Report is a quarterly filing made by companies to report their " +
+                    "performance. This can have their net income, earnings per share, earnings from different " +
+                    "operations, and net sales.\n" +
+                    "With this information you can determine if a company is a good investment or not. \n" +
+                    "These announcements can make the price of the stock move sometimes drastically. ",
+            "Dividends\n\n" +
+                    "A dividend is basically a reward for owning shares in a company. Not all companies" +
+                    " give out dividends. There are companies that give them out, and you can buy their shares. " +
+                    "Dividends are paid out every quarter which is every 3 months. \n" +
+                    "The dividend payout depends on how much the company wants to give out. \n" +
+                    "EX. $T $VALE $IBM $OHI $PSEC "
+    };
+
+    public static String[] description= new String[]{
+            "In dividends we are.... ", "dkgfkbgsdf" ,"fdgsdfg","hsthrtyhrth",
+            "thtjrtjrtjrt", "sdjgbejbg"
     };
 }
